b66ed8913730fbef5d28d49f8bd6e5aad21d99c2
bece21-mkenani <mphatsokenani0@gmail.com>
2025-05-28 18:00:44 +0200

    worked on 2 files

--- js/html.js
+++ js/html.js
@@ -1,8 +1,7 @@
 const popup = document.getElementById("popup");
 const navMobile = document.getElementById("navbar");
 const opacity = document.getElementById("opacity");
-const underc1 = document.getElementById("underc1");
-const underc2 = document.getElementById("underc2");
+
 
 
 popup.addEventListener("click", () => {
@@ -15,12 +14,4 @@ opacity.addEventListener("click", () => {
     navMobile.classList.toggle("active");
     popup.classList.toggle("active")
     opacity.classList.toggle("active")
-});
-
-
-underc1.addEventListener("click", () => {
-    alert("page-under-construction!!");
-});
-underc2.addEventListener("click", () => {
-    alert("page-under-construction!!");
 });
